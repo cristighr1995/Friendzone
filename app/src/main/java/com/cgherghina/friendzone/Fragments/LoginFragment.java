7408bf456cadc79f3a6d8c4974e5adfcff74117b
Cristian-Cătălin GHERGHINA (66884) <cgherghina@stud.acs.upb.ro>
2018-03-30 21:07:07 +0300

    First attempt to login with facebook

--- app/src/main/java/com/cgherghina/friendzone/Fragments/LoginFragment.java
+++ app/src/main/java/com/cgherghina/friendzone/Fragments/LoginFragment.java
@@ -1,16 +1,57 @@
 package com.cgherghina.friendzone.Fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.cgherghina.friendzone.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 public class LoginFragment extends Fragment implements View.OnClickListener {
-    private ImageButton button_loginWithFacebook;
+    private LoginButton button_loginWithFacebook;
+    private CallbackManager callbackManager;
+    private static final String EMAIL = "email";
+
+    private boolean loggedIn = false;
+
+    public void registerFacebookManager(View v) {
+        callbackManager = CallbackManager.Factory.create();
+
+        button_loginWithFacebook = (LoginButton) v.findViewById(R.id.button_loginWithFacebook);
+        button_loginWithFacebook.setReadPermissions(Arrays.asList(EMAIL));
+        button_loginWithFacebook.setFragment(this);
+
+        // Callback registration
+        button_loginWithFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Toast.makeText(getActivity().getApplicationContext(), "Loged in with facebook", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+    }
 
     public LoginFragment() {
         // Required empty public constructor
@@ -19,6 +60,9 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        loggedIn = AccessToken.getCurrentAccessToken() == null;
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
     }
 
     @Override
@@ -26,9 +70,7 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_login, container, false);
-
-        // Get Widgets refferences
-        button_loginWithFacebook = v.findViewById(R.id.button_loginWithFacebook);
+        registerFacebookManager(v);
 
         return v;
     }
@@ -36,11 +78,14 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.button_loginWithFacebook:
-
-                break;
             default:
                 break;
         }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
